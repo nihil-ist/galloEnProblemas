27c43453e13a2184cd78e1fca004a8749003274d
Diego Zamora Delgadillo <al261855@edu.uaa.mx>
2024-10-23 00:49:31 -0600

    ingresarNombre

--- src/app/juego/juego.component.ts
+++ src/app/juego/juego.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-juego',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './juego.component.html',
   styleUrl: './juego.component.css'
 })
 export class JuegoComponent {
+  nombreUsuario: string = '';
 
+  empezarJuego() {
+    console.log("Nombre del usuario:", this.nombreUsuario);
+  }
 }
