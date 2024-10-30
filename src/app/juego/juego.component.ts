050f8df57b0f85e2c1de3ca7356c9d2c50ae959e
diego zamora <al261855@edu.uaa.mx>
2024-10-30 09:04:05 -0600

    nose

--- src/app/juego/juego.component.ts
+++ src/app/juego/juego.component.ts
@@ -12,32 +12,27 @@ import { Router, RouterModule, RouterOutlet } from '@angular/router';
 })
 export class JuegoComponent {
   nombreUsuario: string = '';
-  nombreGuardado: string | null = ''; // Variable para almacenar el nombre guardado
-  mostrarNuevoInput: boolean = false; // Controla si mostrar el input para un nuevo nombre
+  nombreGuardado: string | null = '';
+  mostrarNuevoInput: boolean = false; 
 
   constructor(private router: Router) {}
 
   ngOnInit() {
-    // Verificar si hay un nombre guardado en el localStorage
     this.nombreGuardado = localStorage.getItem('nombreUsuario');
   }
 
   empezarJuego() {
     if (this.mostrarNuevoInput) {
-      // Si el usuario decidió ingresar un nuevo nombre, se guarda y continúa
       localStorage.setItem('nombreUsuario', this.nombreUsuario);
       console.log("Nombre del usuario guardado en localStorage:", this.nombreUsuario);
     } else {
-      // Si el usuario continúa con el nombre guardado
       console.log("Continuando con el nombre guardado:", this.nombreGuardado);
     }
-    // Redirige al componente del juego
     this.router.navigate(['/juego1']);
   }
 
-  // Permite cambiar a la opción de ingresar un nuevo nombre
   ingresarOtroNombre() {
     this.mostrarNuevoInput = true;
-    this.nombreUsuario = ''; // Reinicia el input para el nuevo nombre
+    this.nombreUsuario = ''; 
   }
 }
