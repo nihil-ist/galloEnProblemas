029fb8def3e47f895759c379d6e3a78b3bdccb9b
Diego Zamora Delgadillo <al261855@edu.uaa.mx>
2024-10-29 21:29:25 -0600

    merge pal coco

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { MenuComponent } from './menu/menu.component';
 import { Juego1Component } from './juego1/juego1.component';
 import { NivelCondicionalesComponent } from './nivel-condicionales/nivel-condicionales.component';
 import { NivelFuncionesComponent } from './nivel-funciones/nivel-funciones.component';
+import { Nivel3Component } from './nivel3/nivel3.component';
 
 export const routes: Routes = [
     {path:'',component:MenuComponent},
@@ -15,5 +16,6 @@ export const routes: Routes = [
     {path: 'opciones' ,component: OpcionesComponent},
     {path: 'juego1', component: Juego1Component},
     {path: 'condicionales', component: NivelCondicionalesComponent},
-    {path: 'funciones', component: NivelFuncionesComponent}
+    {path: 'funciones', component: NivelFuncionesComponent},
+    {path: 'ciclos', component: Nivel3Component}
 ];
